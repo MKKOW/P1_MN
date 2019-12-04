aa0a70513693de3b48611c46e8a7694b71f923dd
MKKOW <56612182+MKKOW@users.noreply.github.com>
2019-12-04 19:16:30 +0100

    Poprawione solve i inv oraz indeksowanie w mdoolittle, usuniecie przypadku gdy u(i,i)=0

--- Kody funkcji/mdoolittle.m
+++ Kody funkcji/mdoolittle.m
@@ -1,5 +1,6 @@
 function [U,L]=mdoolittle(A)
 %Wyznacza rozk³ad UL maicerzy A zmodyfikowan¹ metod¹ Doolittle'a
+%Gdy det(A)=0 to metoda nie zadzia³a
 %[U,L]=mdoolittle(A)
    if nargin<1
         error('Not enough input arguments.');
@@ -13,14 +14,16 @@ function [U,L]=mdoolittle(A)
     U=zeros(size(A));
     L=zeros(size(A));
     n=size(A,1);
-    for i=0:(n-1)
-        j=n-i;
-        U(1:j,j)=A(1:j,j)-U(1:j,(n-i+1):n)*L((j+1):n,j);
-        if U(j,j)~=0
-            L(j,1:j)=(A(j,1:j)-U(j,(j+1):n)*L((j+1):n,1:j))./U(j,j);
-        else
-            L(j,j)=1;
-            L(j,1:(j-1))=(A(j,1:(j-1))-U(j,(j+1):n)*L((j+1):n,1:(j-1)));
-        end
+    for j=n:-1:1
+        U(1:j,j)=A(1:j,j)-U(1:j,(j+1):n)*L((j+1):n,j);
+        L(j,1:j)=(A(j,1:j)-U(j,(j+1):n)*L((j+1):n,1:j))./U(j,j);
+       % Gdy "wyskoczy" u=0 na przek¹tnej to ta metoda nie zadzia³a
+       % poni¿ej proponowana modyfkacja
+       % if U(j,j)~=0
+       %    L(j,1:j)=(A(j,1:j)-U(j,(j+1):n)*L((j+1):n,1:j))./U(j,j);
+       % else
+       %    L(j,j)=1;
+       %    L(j,1:(j-1))=(A(j,1:(j-1))-U(j,(j+1):n)*L((j+1):n,1:(j-1)));
+       %end
     end
 end
